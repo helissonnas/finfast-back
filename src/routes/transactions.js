65ed8ce2ae20271d7286205f3e811c4d669dd2ac
helissonnas <helissonnas@MacBook-Air-de-Helisson.local>
2022-06-22 18:57:56 -0300

    paginacao

--- src/routes/transactions.js
+++ src/routes/transactions.js
@@ -4,10 +4,24 @@ import TransactionClass from '../domains/TransactionClass';
 import User from '../domains/User';
 
 const router = Router();
+const DEFAULT_PAGE_SIZE = 2;
 
-router.get('/', (_, res) => {
-  Transaction.findAll({ include: TransactionClass }).then(
-    (result) => res.send(result),
+router.get('/', (req, res) => {
+  const { query } = req;
+
+  let validatedPageSize = query.page_size;
+  const validatedPage = query.page ?? 0;
+
+  if (!query.page_size || query.page_size > 100) {
+    validatedPageSize = DEFAULT_PAGE_SIZE;
+  }
+
+  Transaction.findAndCountAll({
+    offset: validatedPage,
+    limit: validatedPageSize,
+    include: TransactionClass,
+  }).then(
+    (result) => res.send({ ...result, current_page: validatedPage }),
     (reason) => res.status(500).send(reason)
   );
 });
